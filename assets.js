e476c462d01220e87f39532d7c65c5b5be8bbc3f
damian080308-pixel <damian080308@gmail.com>
2026-08-10 17:29:03 -0300

    Update assets.js
    aa

--- assets.js
+++ assets.js
@@ -1,1 +1,49 @@
+const API_URL =
+    "https://script.google.com/macros/s/AKfycbz478Hmfl0suEV6ozgmZfm6k6ptatQsPL4eaxkpnpP1me98ZQyIeIPrnwRUgI7n8wPx/exec";
 
+async function probar(){
+
+    const r =
+        await fetch(
+
+            API_URL,
+
+            {
+                method:"POST",
+
+                headers:{
+                    "Content-Type":
+                        "application/json"
+                },
+
+                body:JSON.stringify({
+
+                    action:
+                        "getData",
+
+                    payload:{}
+
+                })
+
+            }
+
+        );
+
+    const data =
+        await r.json();
+
+    console.log(data);
+
+    document
+        .getElementById(
+            "out"
+        )
+        .textContent =
+
+            JSON.stringify(
+                data,
+                null,
+                2
+            );
+
+}
